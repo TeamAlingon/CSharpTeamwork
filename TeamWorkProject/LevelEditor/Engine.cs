234f5694b606f8902d9f64b99d6f7a27e15acd52
sholev <asen.sholev@gmail.com>
2016-06-03 00:56:03 +0300

    Testing initial LevelEditor ideas.

---
+++ TeamWorkProject/LevelEditor/Engine.cs
@@ -0,0 +1,98 @@
+﻿namespace LevelEditor
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework.Input;
+
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Engine : Game
+    {
+        private Panel proofOfConcept;
+
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Engine()
+        {
+            this.graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            this.proofOfConcept = new Panel(
+                new Vector2(100, 100),
+                new Rectangle(0, 0, 512, 512),
+                this.Content.Load<Texture2D>("UiTiles/GrayTile"),
+                this.Content.Load<SpriteFont>("Fonts/impact"));
+
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            this.IsMouseVisible = true;
+
+            this.graphics.PreferredBackBufferWidth = 1280;
+            this.graphics.PreferredBackBufferHeight = 720;
+            this.graphics.ApplyChanges();
+
+            // Create a new SpriteBatch, which can be used to draw textures.
+            this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            this.proofOfConcept.Draw(gameTime, this.spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TeamWorkProject/LevelEditor/Panel.cs
@@ -0,0 +1,57 @@
+﻿namespace LevelEditor
+{
+    using System.Collections.Generic;
+
+    using LevelEditor.Utils;
+
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class Panel
+    {
+        public Vector2 Position { get; set; }
+
+        public Rectangle Placement { get; set; }
+
+        public Texture2D PanelSize { get; set; }
+
+        public SpriteFont SpriteFont { get; set; }
+
+        private IEnumerable<string> LevelTileMapFilenames { get; set; }
+
+        public Panel(Vector2 position, Rectangle placement, Texture2D panelSize, SpriteFont spriteFont)
+        {
+            this.Position = position;
+            this.Placement = placement;
+            this.PanelSize = panelSize;
+            this.SpriteFont = spriteFont;
+
+            this.LevelTileMapFilenames = FileUtils.GetFilenames("Level");
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.LinearWrap, null, null);
+            spriteBatch.Draw(
+                this.PanelSize,
+                this.Position,
+                this.Placement,
+                Color.White,
+                0,
+                Vector2.Zero,
+                1f,
+                SpriteEffects.None,
+                0);
+
+            var x = this.Position.X + 5;
+            var y = this.Position.Y - 19;
+
+            foreach (string levelTileMapFilename in LevelTileMapFilenames)
+            {
+                spriteBatch.DrawString(this.SpriteFont, levelTileMapFilename, new Vector2(x, y += 19), Color.Black);
+            }
+
+            spriteBatch.End();
+        }
+    }
+}
